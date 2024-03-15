9c6a0655c94d95df3375603ea5d6ced4d43b0db6
Seytkumar_Daniyar <seytkumar980@gmail.com>
2024-03-15 19:10:52 +0600

    создал 2 js для удобного чтения кода

---
+++ js/index.js
@@ -0,0 +1,25 @@
+const Dragon = {
+    name: 'Дракона',
+    hp: 2000, // Жизненная энергия
+    defense: 120, // Защита
+    str: 150, // Сила
+    weapon: 0, // Оружие
+    modifyHelthDragon: modifyHelthDragon,
+    showInfo: showInfo,
+    attackDragon: attackDragon,
+};
+
+const Hero = {
+    name: 'Героя',
+    hp: 1000,
+    defense: 100,
+    str: 120,
+    weapon: 250,
+    shield: 150, // Щит
+    showInfo: showInfo,
+    modifyHelth: modifyHelth,
+    attack: attack,
+};
+
+
+
---
+++ js/utilts.js
@@ -0,0 +1,56 @@
+// function getRandomInt (value) {
+//     return Math.random() < (value / 100)
+// }
+
+const  getRandomInt = (value) => Math.random() < (value / 100);
+
+const showInfo = function () {
+    const message = `HP у ${this.name} ${this.hp}`
+    console.log(message);
+}
+
+const modifyHelth = function () {
+    const damage = this.str + this.weapon - Dragon.defense;
+    Dragon.hp -= damage;
+    const message = `Герой попал по дракону, урон => ${damage}`
+    console.log(message);
+}
+
+const attack = function () {
+       
+    if (getRandomInt (75) ) {
+        this.modifyHelth();
+        Dragon.showInfo();
+        console.log('=====================');
+    } else {
+        const message = `Герой промахнулся =(\n====================`;
+        console.log(message);
+        
+    }
+} 
+
+const modifyHelthDragon = function () {
+    const damageDragon = this.str + this.weapon - Hero.defense
+    Hero.hp -= damageDragon
+    const messageDrakon = `Дракон попал по герою, урон ${damageDragon}`
+    console.log(messageDrakon);
+}
+
+const attackDragon = function () {
+    if (getRandomInt (50) ) {
+        this.modifyHelthDragon();
+        Hero.showInfo();
+        console.log('================');
+    } else {
+        const messageDrakon = `Дракон ушел спать :( \n==========`;
+        console.log(messageDrakon);
+    }
+}
+
+
+
+
+
+ 
+
+
